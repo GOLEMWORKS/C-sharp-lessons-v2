62a403302cea6f2540fdf1ba5e8fb74b5d32d996
K Vladimir <korotkih.vladimir.isr21.1@gmail.com>
2022-07-09 16:25:33 +0200

    Объявление и инициализация переменных

---
+++ Объявление и инициализация переменных/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Объявление_и_инициализация_переменных
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
