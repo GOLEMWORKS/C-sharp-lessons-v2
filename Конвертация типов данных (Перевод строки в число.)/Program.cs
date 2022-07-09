0b2357e11a73d8c140e60c73b374238f834dec4d
K Vladimir <korotkih.vladimir.isr21.1@gmail.com>
2022-07-09 20:27:40 +0200

    Конвертация строки в другие типы данных

---
+++ Конвертация типов данных (Перевод строки в число.)/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Конвертация_типов_данных__Перевод_строки_в_число._
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //конвертация строки в другие типы данных
+
+
+        }
+    }
+}
