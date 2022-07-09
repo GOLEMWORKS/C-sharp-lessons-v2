b82ba2abfd366a772bb0682a75cd089db76144b8
K Vladimir <korotkih.vladimir.isr21.1@gmail.com>
2022-07-09 13:05:21 +0200

    Команды для справки по особенностям определённых типов данных

--- Типы данных/Program.cs
+++ Типы данных/Program.cs
@@ -10,7 +10,41 @@ namespace Типы_данных
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Типы данных в C#");
+            Console.Title = "Типы данных в C#";
+
+            Console.WriteLine("Целые типы: sbyte, byte, short, ushort, int, uint, long, ulong." +
+                "\n\nТипы, в которых сохраняются данные с плавающей запятой(соответствуют вещественному типу): float, double, decimal." +
+                "\n\nСимвольный тип: char." +
+                "\n\nЛогический тип: bool." +
+                "\n\nСтрочный тип: string.");
+
+            //Cтоит всегда учитывать размер памяти каждой комманды, выполняемой программой для лучшей производительности и меньшего веса!
+            Console.WriteLine("\nДля определения размера переменной заданного типа используется sizeof():");
+
+            int d;
+            d = sizeof(sbyte); // d = 1
+            Console.WriteLine("sbyte:" + d);
+
+            d = sizeof(short); // d = 2
+            Console.WriteLine("short:" + d);
+
+            d = sizeof(int);   // d = 4
+            Console.WriteLine("int:" + d);
+
+            d = sizeof(long);  // d = 8
+            Console.WriteLine("long:" + d);
+
+            Console.WriteLine("\nОпределение минимального и максимального значения производится через .MaxValue");
+
+            int i;
+            int MaxI;
+            int MinI;
+
+            MaxI = int.MaxValue; // MaxI =  2147483647
+            Console.WriteLine("Максимальное значение: " + MaxI);
+
+            MinI = int.MinValue; // MinI = -2147483648
+            Console.WriteLine("Минимальное значение: " + MinI);
         }
     }
 }
