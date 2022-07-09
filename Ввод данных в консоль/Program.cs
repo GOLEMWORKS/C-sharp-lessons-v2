271829bbbfa5d60c1fcf11d3011c791de85d3ccd
K Vladimir <korotkih.vladimir.isr21.1@gmail.com>
2022-07-09 18:39:04 +0200

    Ввод данных в консоль

---
+++ Ввод данных в консоль/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ввод_данных_в_консоль
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
