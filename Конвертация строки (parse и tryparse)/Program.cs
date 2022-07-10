9e5b9ac92fe9a2833d3f9231d74a0c8695a46bf6
K Vladimir <korotkih.vladimir.isr21.1@gmail.com>
2022-07-10 19:03:11 +0200

    Parse  и Tryparse

---
+++ Конвертация строки (parse и tryparse)/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Конвертация_строки__parse_и_tryparse_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
