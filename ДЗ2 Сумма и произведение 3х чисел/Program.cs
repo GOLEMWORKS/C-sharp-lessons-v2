c0899cd84e0ad23f536d7667df9214bed823c032
K Vladimir <korotkih.vladimir.isr21.1@gmail.com>
2022-07-13 16:10:11 +0200

    FIX ДЗ2

--- ДЗ2 Сумма и произведение 3х чисел/Program.cs
+++ ДЗ2 Сумма и произведение 3х чисел/Program.cs
@@ -13,51 +13,73 @@ namespace ДЗ2_Сумма_и_произведение_3х_чисел
             double a, b, c;
             string str;
 
-            Console.WriteLine(" Сумма и произведение трёх чисел ");
+            Console.WriteLine("Сумма и произведение трёх чисел ");
             Console.WriteLine("Введите первое число: ");
             str = Console.ReadLine();
 
             bool result = double.TryParse(str, out a);
-            
-            if(result)
+
+            //проверка на возможность перевести строку к типу double
+            if (result)
             {
                 Console.WriteLine("Введите второе число: ");
                 str = Console.ReadLine();
 
-                bool inres1 = double.TryParse(str, out b);
-
-                if (inres1) 
+                //Проверка на деление на ноль
+                if (str == "0")
                 {
-                    Console.WriteLine("Введите третье число: ");
-                    str = Console.ReadLine();
-
-                    bool inres2 = double.TryParse(str, out c);
-
-                    if (inres2)
-                    {
-                        double sum, mult, dif, div;
-
-                        sum = a + b + c;
-                        mult = a * b * c;
-                        dif = a - b - c;
-                        div = a / b / c;
-
-                        Console.WriteLine("Сумма чисел: " + sum + "\nПроизведение чисел: " + mult);
-                    }
-
-                    else
-
-                    {
-                        Console.WriteLine("Строка со значением " + str + " не является числом!");
-                    }
+                    Console.WriteLine("Данная переменная не может принимать значение 0!");
                 }
 
                 else
                 {
-                    Console.WriteLine("Строка со значением " + str + " не является числом!");
+
+                    bool inres1 = double.TryParse(str, out b);
+
+                    //проверка на возможность перевести строку к типу double
+                    if (inres1)
+                    {
+                        Console.WriteLine("Введите третье число: ");
+                        str = Console.ReadLine();
+
+                        //Проверка на деление на ноль
+                        if (str == "0")
+                        {
+                            Console.WriteLine("Данная переменная не может принимать значение 0!");
+                        }
+
+                        else
+                        {
+                            bool inres2 = double.TryParse(str, out c);
+                            //проверка на возможность перевести строку к типу double
+                            if (inres2)
+                            {
+                                double sum, mult, dif, div;
+
+                                sum = a + b + c;
+                                mult = a * b * c;
+                                dif = a - b - c;
+                                div = a / b / c;
+
+                                Console.WriteLine("Сумма чисел: " + sum + "\nПроизведение чисел: " + mult + "\nРазность чисел: " + dif + "\nЧастное чисел: " + div);
+                            }
+
+                            else
+
+                            {
+                                Console.WriteLine("Строка со значением " + str + " не является числом!");
+                            }
+
+                        }
+
+                    }
+
+                    else
+                    {
+                        Console.WriteLine("Строка со значением " + str + " не является числом!");
+                    }
                 }
             }
-
             else
 
             {
