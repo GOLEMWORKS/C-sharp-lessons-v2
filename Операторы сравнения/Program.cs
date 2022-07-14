6b8bf5f285ab523e61fc213c071c5014c4aa6c22
K Vladimir <korotkih.vladimir.isr21.1@gmail.com>
2022-07-14 14:59:25 +0200

    Операторы сравнения и операции с нимим (Файл и коммент)

---
+++ Операторы сравнения/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Операторы_сравнения
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+                    > Больше
+                    < Меньше
+                    == Равно
+                    != НЕ равно
+                    >= Меньше или равно
+                    <= Больше иили равно
+             
+             */
+        }
+    }
+}
