a6a8aa8b61cd8967daeb6b27e9b251aefa8cb98a
K Vladimir <korotkih.vladimir.isr21.1@gmail.com>
2022-07-15 19:45:50 +0200

    ДЗ4 Проверка вводимого числа на чётность

---
+++ ДЗ4 Проверка чисел на чётность/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace ДЗ4_Проверка_чисел_на_чётность
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string str;
+            int a, div;
+
+            Console.WriteLine("Введите целое число:");
+            
+            str = Console.ReadLine();
+
+            bool result = int.TryParse(str, out a);
+            
+            if (result)
+            {
+                div = a % 2;
+
+                if(div == 0)
+                {
+                    Console.WriteLine("Число чётное");
+                }
+                
+                else
+                {
+                    Console.WriteLine("Число не чётное");
+                }
+            }
+
+            else
+            {
+                Console.WriteLine("Вы ввели не целое число");
+            }
+        }
+    }
+}
