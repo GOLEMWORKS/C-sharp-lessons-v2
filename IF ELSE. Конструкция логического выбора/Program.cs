ac691d9641b89f50cbb537a17449a5942a25f209
K Vladimir <korotkih.vladimir.isr21.1@gmail.com>
2022-07-15 14:11:04 +0200

    IF ELSE Проект

---
+++ IF ELSE. Конструкция логического выбора/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace IF_ELSE._Конструкция_логического_выбора
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             *  if(Утверждение или выражение) 
+             *  {
+             *      Действие 1
+             *  }
+             * else
+             * {
+             *      Действие 2
+             * }
+             */
+        }
+    }
+}
