c524806c4896312cf0409f62355878aad382e376
K Vladimir <korotkih.vladimir.isr21.1@gmail.com>
2022-07-16 19:35:23 +0200

    case и блок default

--- Условный оператор Switch/Program.cs
+++ Условный оператор Switch/Program.cs
@@ -4,7 +4,25 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+           int a = int.Parse(Console.ReadLine());
+
+            switch (a)
+            {
+                //Если значение переменной a = 1
+                case 1:
+                    Console.WriteLine("Вы ввели число " + a);
+                    break;
+
+                case 2:
+                    Console.WriteLine("Вы ввели число " + a);
+                    break;
+                default:
+                    Console.WriteLine("Введите либо единицу либо двойку!");
+                    break;
+            }
+            /*Если switch не находит совпадений в своих кейсах с тем, что мы ему передали, то он ничего не будет делать и просто пропустит код,НО!!!
+              Блок default позволит выполнить определённые операции с входными данными, даже если они не соответствуют кейсам!
+             */
         }
     }
 }
