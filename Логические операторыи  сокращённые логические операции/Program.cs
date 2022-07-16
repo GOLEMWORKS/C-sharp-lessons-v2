33c62bb83d77dd5405d828515e432aa77230e714
K Vladimir <korotkih.vladimir.isr21.1@gmail.com>
2022-07-16 19:22:07 +0200

    Финальная версия

--- Логические операторыи  сокращённые логические операции/Program.cs
+++ Логические операторыи  сокращённые логические операции/Program.cs
@@ -25,15 +25,29 @@
                 Console.WriteLine(" Не является оружием ");
             }
 
-            
-            //Cокращённый И " && "
-            bool isTemperature = true;
-            bool isCooling = true;
 
+            //Cокращённый И " && "
+            double temp, speed;
+            temp = 190.21; //температура
+            speed = 2902; //обороты
+
+            bool isTemperature = temp >= 100;
+            bool isCooling =  speed <= 2000;
+
+            //Работает только когда оба элемента имеют значение true
             if(isTemperature && isCooling)
             {
-                Console.WriteLine(" Замечен высокий показатель температуры прибора! ");
+                Console.WriteLine(" Сбой в работе прибора! ");
             }
+            //Аналогичная программка с сокращённым или ||. Активируется когда один из элементов true
+            if (isTemperature || isCooling)
+            {
+                Console.WriteLine(" Сбой в работе прибора! isTemperature: " + isTemperature + " , isCooling: " + isCooling);
+            }
+            /*Cокращённые и не сокращённые отличаются тем, что первые не проверяют вторую часть условия, если в этом нет необходимости.
+              Не сокращённые версии использвуются редко из-за специфики их работы и большего времени на обработку!  
+            */
+
 
             Console.ReadLine();
         }
