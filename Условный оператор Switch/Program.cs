a82ea774587a50e24d9adf7a3aa3400d6ab55bc0
K Vladimir <korotkih.vladimir.isr21.1@gmail.com>
2022-07-16 19:24:10 +0200

    Условный оператор Switch, создание проекта

---
+++ Условный оператор Switch/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Условный_оператор_Switch
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
