6e2e260d7c0afd492a632752f2b34d96502a6948
K Vladimir <korotkih.vladimir.isr21.1@gmail.com>
2022-07-16 22:56:19 +0200

    Калькулятор на switch файл

---
+++ ДЗ5.1 Калькулятор на switch/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace ДЗ5._1_Калькулятор_на_switch
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите первое число:");
+            
+        }
+    }
+}
