ab3cb64dedb05546bf249a37d7f3bb2c2876b73b
K Vladimir <korotkih.vladimir.isr21.1@gmail.com>
2022-07-17 21:24:48 +0200

    Цикл while и 1000-7

---
+++ Цикл while/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace Цикл_while
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int count = 1000;
+            while (count > 0)//Выполняется до тех пор пока выполняется условие в скобках
+            {
+                count = count - 7;
+                Console.WriteLine(count);
+            }
+        }
+    }
+}
