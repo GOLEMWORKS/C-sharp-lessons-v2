fd848d3b2cf12fde543ff88d1fcf6e0747cafe0c
K Vladimir <korotkih.vladimir.isr21.1@gmail.com>
2022-07-17 18:49:30 +0200

    v 1.0 С проверкой деления на ноль

--- ДЗ5.1 Калькулятор на switch/Program.cs
+++ ДЗ5.1 Калькулятор на switch/Program.cs
@@ -4,7 +4,73 @@
     {
         static void Main(string[] args)
         {
+            double a, b;
+            bool res;
+            string ins;
+
+
             Console.WriteLine("Введите первое число:");
+            ins = Console.ReadLine();
+
+            res = double.TryParse(ins, out a);
+
+            if (res)
+            {
+
+                Console.WriteLine("Введите вторе число:");
+                ins = Console.ReadLine();
+
+                res = double.TryParse(ins, out b);
+                if(res)
+                {
+
+                    Console.WriteLine("Выберите оператор и введите его в консоль:\n + ; - ; / ; *");
+                    ins = Console.ReadLine();
+
+                    switch(ins)
+                    {
+                        case "+":
+                            Console.WriteLine("Сумма чисел: " + (a + b));
+                            break;
+
+                        case "-":
+                            Console.WriteLine("Разность чисел: " + (a - b));
+                            break;
+
+                        case "/":
+                            if (b != 0)
+                            {
+
+                                Console.WriteLine("Частное чисел: " + (a / b));
+                            }
+
+                            else
+                            {
+                                Console.WriteLine("Делитель (Второе число) не может быть равен нулю!");
+                            }
+                            break;
+
+                        case "*":
+                            Console.WriteLine("Произведение чисел: " + (a * b));
+                            break;
+
+                        default:
+                            Console.WriteLine("Вы ввели неверный оператор!");
+                            break;
+                    }
+                }
+                
+                else
+                {
+                    Console.WriteLine("Вы ввели некорректный тип данных!");
+                }
+
+            }
+
+            else
+            {
+                Console.WriteLine("Вы ввели некорректный тип данных!");
+            }
             
         }
     }
