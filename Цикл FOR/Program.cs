70317b1deb742799513885176eddc5ed4082645f
K Vladimir <korotkih.vladimir.isr21.1@gmail.com>
2022-07-18 14:15:04 +0200

    Цикл FOR. 1000-7 в цикле

---
+++ Цикл FOR/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace Цикл_FOR
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Улучшенная версия цикла while, но всё зависит от случая!
+
+
+            //1000-7 через цикл for
+            for (int i = 1000; i > 0; i -= 7) // i - счётчик(итератор), i < lenght - ограничение, условие
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
