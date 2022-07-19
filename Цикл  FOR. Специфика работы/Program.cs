e536bf673b78cbf4f60d7831a9f25b59264d095d
K Vladimir <korotkih.vladimir.isr21.1@gmail.com>
2022-07-19 18:31:30 +0200

    Доп. Инфа о цикле for

---
+++ Цикл  FOR. Специфика работы/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Цикл__FOR._Специфика_работы
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
