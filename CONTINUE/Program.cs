7d1ab4120480ad97fb8cad1b4f61428716f1bd7b
K Vladimir <korotkih.vladimir.isr21.1@gmail.com>
2022-07-20 13:06:10 +0200

    Ключевое слово continue

---
+++ CONTINUE/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace CONTINUE
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // continue позволяет пропутить итерацию цикла, при этом не выходя из него!
+
+            for (int i = 0; i < 5; i++)
+            {
+                if (i == 2)
+                {
+                    continue; // Пропуск итерации двойки
+                }
+
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
