ee06fa946a9d918bd30a878cce298566014b3b1c
K Vladimir <korotkih.vladimir.isr21.1@gmail.com>
2022-07-20 12:59:04 +0200

    Ключевое слово break

---
+++ BREAK/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace BREAK
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //break прерывает любой цикл
+
+            for (int i = 0; i < 100; i++)
+            {
+                string msg = Console.ReadLine();
+                if (msg == "exit")
+                {
+                    break;
+                }
+
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
