efd028a7273aaaabfdedd9f4d2348459404ebeb2
K Vladimir <korotkih.vladimir.isr21.1@gmail.com>
2022-07-25 13:49:39 +0200

    Взаимодействие массивов и ДЗ8

---
+++ Взаимодействие массивов и циклов. Вывод массивов/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace Взаимодействие_массивов_и_циклов._Вывод_массивов
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] myArray = { 1,2,412,123,21,312,321,12};
+
+            Console.WriteLine("№ | Число");
+
+            for (int i = 0; i < myArray.Length; i++)
+            {
+                Console.WriteLine(i + ": " + myArray[i]);
+            }
+        }
+    }
+}
---
+++ ДЗ8 Массивы/Program.cs
@@ -0,0 +1,59 @@
+﻿namespace ДЗ8_Массивы
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        while(true) 
+            {
+                Console.Clear();
+                Console.WriteLine("Список заданий:" +
+                "\n1. Ввод в массив чисел с клавиатуры" +
+                "\n2. Вывод массива в обратном порядке" +
+                "\n3. Сумма чётных чисел в массиве" +
+                "\n4. Наименьшее число в массиве");
+                Console.WriteLine("Введите номер интересующего задания:");
+                switch (Console.ReadLine())
+                {
+                    case "1": //Ввод чисел в массив с клавиатуры:
+                        try
+                        {
+                            int length;
+                            double n;
+
+                            Console.WriteLine("Введите длину массива(целое число!!!): ");
+                            length = int.Parse(Console.ReadLine());
+
+                            double[]firstArray = new double[length];// метод объявления массива в цикле for
+
+                            for (int i = 0; i < length; i++)
+                            {
+                                Console.WriteLine("Введите элемент под номером " + i);
+                                firstArray[i] = double.Parse(Console.ReadLine());
+                            }
+                            Console.WriteLine("--------------Ваш массив:----------------");
+                            for(int i = 0; i < firstArray.Length; i++)
+                            {
+                                Console.WriteLine(i + ": " + firstArray[i]);
+                            }
+                            Console.ReadLine();
+
+                        }
+                        catch (Exception)
+                        {
+                            Console.WriteLine("Вы ввели не числовое значение! \nНажмите enter для перезапуска программы.");
+                            Console.ReadLine();
+                            continue;
+                        }
+                        break;
+
+
+                }
+
+            }
+
+
+
+        }
+    }
+}
