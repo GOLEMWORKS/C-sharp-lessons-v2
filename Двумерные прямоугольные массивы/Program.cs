fce742f74d4567c410d15c3499fd363b3831c1cc
K Vladimir <korotkih.vladimir.isr21.1@gmail.com>
2022-07-27 19:51:10 +0200

    Вывод двумерного массива в консоль

--- Двумерные прямоугольные массивы/Program.cs
+++ Двумерные прямоугольные массивы/Program.cs
@@ -21,10 +21,34 @@
                 {1, 2,13,4,1},
                 {12,42,4,5,98 },
                 {12,42,4,5,98 },
+                {1, 2,13,4,1},
                 {12,42,4,5,98 }
             };
 
-            Console.WriteLine("Инициализированный массив");
+            //вывод двумерного массива строкой
+
+            foreach (var item in twoDimensionsArray2) //Цикл для вывода всех элементов коллекции
+            {
+                Console.Write(item + " ");
+            }
+
+            //вывод двумерного массива табличкой
+            Console.WriteLine(twoDimensionsArray2.Rank); // .Rank - показывает мерность массива
+            Console.WriteLine("Количество эл-в по вертикали:" + twoDimensionsArray2.GetLength(0)); // .GetLength - позволяет узнать к-во элементов в измерении. Нумерация начинается с нуля, поэтому первое измерение = 0, второе = 1
+            Console.WriteLine("Количество эл-в по горизонтали:" + twoDimensionsArray2.GetLength(1));
+
+            int ArrayHeight = twoDimensionsArray2.GetLength(0);
+            int ArrayWidth = twoDimensionsArray2.GetLength(1);
+
+            Console.WriteLine("Вывод массива twoDimensionsArray2");
+            for (int y = 0; y < ArrayHeight; y++) // Вывод элементов по вертикали
+            {
+                for (int x = 0; x < ArrayWidth; x++) //Вывод элементов по горизонтали
+                {
+                    Console.Write(twoDimensionsArray2[y,x] + "\t"); // y - ряд; x - элемент в текущем ряду
+                }
+                Console.WriteLine();
+            }
         }
     }
 }
