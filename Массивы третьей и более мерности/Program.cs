7844aadcecb9425dfd5e448b4430c69b4a327f69
K Vladimir <korotkih.vladimir.isr21.1@gmail.com>
2022-07-29 17:15:43 +0200

    Вывод многомерного массива test

--- Массивы третьей и более мерности/Program.cs
+++ Массивы третьей и более мерности/Program.cs
@@ -4,7 +4,63 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            int[,,] newThreeDimArray = new int[4,3,5];
+
+            //Инициализация вручную:
+            int[,,] secondThreeDimArray = 
+            {
+                {
+                    {1,2,5 },
+                    {3,6,2 },
+                    {2,7,9 }
+                },
+                {
+                    {6,2,1 },
+                    { 8,4,3},
+                    { 7,2,5}
+                },
+                {
+                    {12,3,5},
+                    { 8,2,9},
+                    { 0,2,4}
+                }
+            };
+
+            //(Чем больше измерений, тем больше циклов)
+
+            //Заполнение массива случайными числами
+            Random rand = new Random();
+
+            for (int i = 0; i < newThreeDimArray.GetLength(0); i++) 
+            {
+                for (int j = 0; j < newThreeDimArray.GetLength(1); j++)
+                {
+                    for (int k = 0; k < newThreeDimArray.GetLength(2); k++)
+                    {
+                        newThreeDimArray[i, j, k] = rand.Next(100);
+                    }
+                }
+            }
+
+
+            //Вывод массива
+
+            for (int i = 0; i < newThreeDimArray.GetLength(0); i++)
+            {
+                Console.WriteLine("Page #" + (i + 1));
+
+                for (int j = 0; j < newThreeDimArray.GetLength(1); j++)
+                {
+                    for (int k = 0; k < newThreeDimArray.GetLength(2); k++)
+                    {
+                        Console.Write(newThreeDimArray[i,j,k] + " ");
+                    }
+                    Console.WriteLine();
+                }
+                Console.WriteLine();
+            }
+
+            Console.ReadLine();
         }
     }
 }
