37cc579b17a912b15724043e513b96d4df11c836
K Vladimir <korotkih.vladimir.isr21.1@gmail.com>
2022-07-29 16:49:14 +0200

    Трёх и более мерные массивы. Файл

---
+++ Массивы третьей и более мерности/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Массивы_третьей_и_более_мерности
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
