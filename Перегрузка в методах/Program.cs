cf3f863c83db20cbdd901e52b2c86e2d303fdcb3
K Vladimir <korotkih.vladimir.isr21.1@gmail.com>
2022-07-31 18:33:25 +0200

    1.0

---
+++ Перегрузка в методах/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Перегрузка_в_методах
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
