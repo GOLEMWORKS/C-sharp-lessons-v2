a2ccdff05e1f43195447aa20c0238c4e4c26bbff
K Vladimir <korotkih.vladimir.isr21.1@gmail.com>
2022-08-04 22:00:46 +0200

    ??= оператор присваивания объединения значения с null

---
+++ Оператор присваивания объединения  значения  с null/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace Оператор_присваивания_объединения__значения__с_null
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //??= - заместо null присваивает иное значение для переменной
+
+            string str = null;
+
+            str ??= string.Empty; //str присваиваеся значение за место null; можно делать всё через одну переменную
+
+            Console.WriteLine("Длина строки: " + str.Length);
+
+            //Пример с массивами 
+            int[] myArr = GetArray(); // данному массиву присваивается значение 0
+
+            myArr ??= new int[0]; //Проверка на null и присвоение массива на ноль элементов
+
+            Console.WriteLine("Кол-во элементов в массиве: " + myArr.Length);
+        }
+
+        static int[] GetArray()
+        {
+            int[] myArrray = null;
+
+            return myArrray;
+        }
+    }
+}
