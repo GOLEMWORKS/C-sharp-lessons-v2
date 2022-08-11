48db48cd610bae79f1ac2ce88e744e177aa6f9e9
K Vladimir <korotkih.vladimir.isr21.1@gmail.com>
2022-08-11 22:09:59 +0200

    Необязательные параметры методов

--- Необязательные параметры метода/Program.cs
+++ Необязательные параметры метода/Program.cs
@@ -2,9 +2,28 @@
 {
     internal class Program
     {
+        static int Sum(int a, int b, bool isEnableLogging = false)//Если значение уже указанно, то при вызове метода этот параметр становится не обязательным!
+        {
+            int result = a + b;
+            //Параметры по умолчанию ВСЕГДА В КОНЦЕ СПИСКА!
+            if (isEnableLogging)
+            {
+                Console.WriteLine("Логирование включено!");
+                Console.WriteLine("Первое слагаемое: " + a);
+                Console.WriteLine("Второе слагаемое: " + b);
+                Console.WriteLine("Результат: " + result);
+            }
+            return result;
+        }
+
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            int.TryParse(Console.ReadLine(), out int firstSum);
+            int.TryParse(Console.ReadLine(), out int secondSum);
+            bool.TryParse(Console.ReadLine(), out bool enableLogging);
+
+            Sum(firstSum, secondSum, enableLogging); 
         }
     }
 }
