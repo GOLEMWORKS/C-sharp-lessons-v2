17c1759fb366cb8dc56b15966ebeac6ebacb635c
K Vladimir <korotkih.vladimir.isr21.1@gmail.com>
2022-08-11 17:46:49 +0200

    do while (пропущенный)

---
+++ Цикл Do While/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace Цикл_Do_While
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Цикл do while и его особенности
+            Console.WriteLine("DO WHILE");
+           /* while (true)
+            {
+                Сначала проверяет условие, а потом выполняет действие
+            }
+           */
+            int count = 5;
+
+            do
+            {
+                //Сначала выполняет действие, а потом проверяет условие 
+                //Выполняется всегда хотябы один раз!
+                count++;
+                Console.WriteLine(count); //Выведет 6
+            } while (count < 5);
+        }
+    }
+}
