8fad5b59476ebb5fbdd3e316e7f044e0b89a38ef
K Vladimir <korotkih.vladimir.isr21.1@gmail.com>
2022-08-11 21:48:11 +0200

    Необязательные параметры методов

---
+++ Необязательные параметры метода/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Необязательные_параметры_метода
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
