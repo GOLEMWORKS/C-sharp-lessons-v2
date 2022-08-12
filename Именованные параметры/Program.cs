957e247bfa3f1678903343f7e9e99798959981af
K Vladimir <korotkih.vladimir.isr21.1@gmail.com>
2022-08-12 15:32:13 +0200

    Именованные параметры, примеры и использование

--- Именованные параметры/Program.cs
+++ Именованные параметры/Program.cs
@@ -2,9 +2,23 @@
 {
     internal class Program
     {
+
+        static void TestMethod(int a, int b)
+        {
+            Console.WriteLine("Переменная a = " + a);
+            Console.WriteLine("Переменная b = " + b);
+        }
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            TestMethod(1, 2); //a = 1, b = 2
+            Console.WriteLine();
+            TestMethod(b: 2, a: 3);//Именновванный параметр. a = 3, b = 2
+            Console.WriteLine();
+
+            int firstNum = 4;
+            int secondNum = 2;
+
+            TestMethod(b: firstNum, a: secondNum);//Можно передавать переменные
         }
     }
 }
