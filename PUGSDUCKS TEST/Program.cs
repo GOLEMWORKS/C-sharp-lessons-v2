90035ed6a677cb6fd68c8d4ec23ffa0dcc3f503f
K Vladimir <korotkih.vladimir.isr21.1@gmail.com>
2022-08-14 23:34:20 +0200

    pugsducks

---
+++ PUGSDUCKS TEST/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace PUGSDUCKS_TEST
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
