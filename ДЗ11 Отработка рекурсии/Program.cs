be2a64157fed9c4936e7c0f6e196c3a39db5fc84
K Vladimir <korotkih.vladimir.isr21.1@gmail.com>
2022-08-16 22:01:14 +0200

    save 1

--- ДЗ11 Отработка рекурсии/Program.cs
+++ ДЗ11 Отработка рекурсии/Program.cs
@@ -16,15 +16,54 @@
                 switch (i)
                 {
                     case 1:
-                        Console.WriteLine(1);
+                        Console.WriteLine("Задание 1: Вывод массива с помощью рекурсии (Массив генерирую рандомно)");
+                        int[] firstTaskArray = RandomIntArray();
+                        ArrayDisplayByRecursion(firstTaskArray);
+
+
+
                         break;
+
+                    case 2:
+                        Console.WriteLine(2);
+                        break;
+
+                    case 3:
+                        Console.WriteLine(3);
+                        break;
+
                     default:
-                        Console.WriteLine("Неизвестный номер задания");
+                        Console.WriteLine(" Неизвестный номер задания");
                         break;
                 }
 
                 Console.ReadLine();
             }
+        }
+
+        static int[] RandomIntArray()
+        {
+            int[] IntArray = new int[12];
+            Random random = new Random();
+
+            for (int i = 0; i < IntArray.Length; i++)
+            {
+                IntArray[i] = random.Next(100);
+            }
+
+            return IntArray;
+        }
+
+        static void ArrayDisplayByRecursion(int[] Array, int i = 0)
+        {
+            Console.Write(":");
+
+            if (i >= Array.Length)
+            {
+                return;
+            }
+            i++;
+            ArrayDisplayByRecursion(Array, i);
         }
     }
 }
