1b8588e48c987a6ca877285c301b9ddd3f46e6c5
K Vladimir <korotkih.vladimir.isr21.1@gmail.com>
2022-08-16 20:49:57 +0200

    Рекурсия_и_Stack_Owerflow

---
+++ Рекурсия и Stack Owerflow/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Рекурсия_и_Stack_Owerflow
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
