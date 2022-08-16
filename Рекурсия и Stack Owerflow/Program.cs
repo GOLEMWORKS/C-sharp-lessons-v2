6a4e2e549cd092b2a3210a58db50986793fbc79f
K Vladimir <korotkih.vladimir.isr21.1@gmail.com>
2022-08-16 21:26:56 +0200

    Рекурсия комменты

--- Рекурсия и Stack Owerflow/Program.cs
+++ Рекурсия и Stack Owerflow/Program.cs
@@ -2,9 +2,30 @@
 {
     internal class Program
     {
+        static void RecursionTestStart()
+        {
+            Console.WriteLine("Recursion test is running!");
+            RecursionTestStart(); //Рекурсия - вызов метода внутри самого себя
+            //Рекурсия ограничена размером стека, поэтому всегда нужно
+            //указывать условия выхода из рекурсии, дабы не нарваться на Stack Owerflow
+            //Лучше рекурсию не использовать, если в методе много переменных и данных, т.к. это влияет на производительность
+        }
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            //RecursionTestStart();
+            RecursionBreak(1);
+        }
+
+        static void RecursionBreak(int i)
+        {
+            Console.WriteLine("Рекурсия идёт уже " + i + " шагов.");
+            if (i >= 4)
+            {
+                return;//Выход из рекурсии при выполнении определённого условия
+            }
+
+            i++;
+            RecursionBreak(i);
         }
     }
 }
