94eed324c2d0bb2b50dfc409464125482c1c430c
K Vladimir <korotkih.vladimir.isr21.1@gmail.com>
2022-08-20 11:23:39 +0300

    преобразование и приведение типов данных создание

---
+++ Преобразование (привидение) типов данных/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Преобразование__привидение__типов_данных
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
