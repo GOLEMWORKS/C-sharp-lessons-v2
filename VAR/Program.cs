85ed23ed22fe26c6e663f6b8bc2ca3be51e0dce7
K Vladimir <korotkih.vladimir.isr21.1@gmail.com>
2022-08-28 17:43:56 +0200

    Применение var

--- VAR/Program.cs
+++ VAR/Program.cs
@@ -4,7 +4,15 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            //var не является типом данных и служит для того, чтобы компилятор мог сам присвоить переменной определённый тип
+           
+            var i = 5; //Целое число типа int
+            var s = "qweqe"; // Строка
+            var a = new float[12]; //Массив типа float
+
+            Console.WriteLine(i.GetType() + " " +
+                              s.GetType() + " " +
+                              a.GetType() + " " );
         }
     }
 }
