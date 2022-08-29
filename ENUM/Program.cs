45bdcd8be325c1090f3c9725e128e01721074d89
K Vladimir <korotkih.vladimir.isr21.1@gmail.com>
2022-08-29 20:54:40 +0200

    Реальное использование enum

--- ENUM/Program.cs
+++ ENUM/Program.cs
@@ -4,7 +4,20 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            //реальное использование
+            while (true)
+            {
+                var key = Console.ReadKey().Key;
+
+                int keyCode = (int)key;
+
+                Console.WriteLine($"\tEnum {key}\tKey Code {keyCode}");
+
+                if (key == ConsoleKey.Enter)
+                {
+                    Console.WriteLine("Нажат Enter");
+                }
+            }
         }
     }
 }
