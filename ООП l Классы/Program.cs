aa07a97242aefae8b80dc30b5ea07cb2252a7779
K Vladimir <korotkih.vladimir.isr21.1@gmail.com>
2022-08-30 20:42:54 +0200

    фиксирование, создание ООП l Классы

---
+++ ООП l Классы/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace ООП_l_Классы
+{
+    internal class Program
+    {
+        //Класс, объект класса, экземпляр класса
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
