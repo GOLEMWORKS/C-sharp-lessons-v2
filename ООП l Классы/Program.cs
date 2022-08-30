18304be68f0f4c444b8b5a6915160a5346df389f
K Vladimir <korotkih.vladimir.isr21.1@gmail.com>
2022-08-30 21:01:20 +0200

    2 объекта класса

--- ООП l Классы/Program.cs
+++ ООП l Классы/Program.cs
@@ -7,15 +7,18 @@
         {
             Point p1 = new Point();//Создания места в куче для объекта данного класса
             //р1 - это объект класса, с которым ведётся непосредственная работа
-
             p1.x = 4;
             p1.y = 2;
+            Console.WriteLine($"X: {p1.x} & Y: {p1.y}");
 
-            Console.WriteLine($"{p1.x} & {p1.y}");
+            var p2 = new Point();
+            p2.x = 2;
+            p2.y = 6;
+            Console.WriteLine($"X: {p2.x} & Y: {p2.y}");
         }
     }
 
-    class Point//ССЫЛОЧНЫЙ ТИП ДАННЫХ
+    class Point//ССЫЛОЧНЫЙ ТИП ДАННЫХ. Шаблон или схема
     {
         public int x; //Внутри класса переменные называются полями класса
         public int y;
