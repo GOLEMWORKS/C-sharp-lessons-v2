9a9f67f0966ae94b6e1aeb6d5447e5e4bc06c83c
K Vladimir <korotkih.vladimir.isr21.1@gmail.com>
2022-09-04 14:23:15 +0200

    Создание Методы объекта класса

---
+++ ООП l Методы объекта класса/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace ООП_l_Методы_объекта_класса
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
