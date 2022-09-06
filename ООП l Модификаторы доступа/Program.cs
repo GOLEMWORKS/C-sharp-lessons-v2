65480f27c08cd4c7e33656f35af77d85416344c2
K Vladimir <korotkih.vladimir.isr21.1@gmail.com>
2022-09-06 18:17:45 +0200

    Модификаторы доступа

---
+++ ООП l Модификаторы доступа/Program.cs
@@ -0,0 +1,40 @@
+﻿namespace ООП_l_Модификаторы_доступа
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Модификаторы public и private (Используются при инкапсуляции)
+            //public - доступно снаружи класса
+            //private - доступно только внутри класса
+            var point = new Point();
+
+            point.PrintY(); //Публичный метод, в котором находится приватная переменная
+            Console.WriteLine();
+            point.PrintPoint(); //Опосредовано вызываем приватный метод PrintX
+        }
+    }
+
+    class Point
+    {
+        public int x = 4;
+        private int y = 44;
+        int z = 12; //По умолчанию используется модификатор private
+
+        private void PrintX() //Вредный пример, нарушающий инкапсуляцию
+        {
+            Console.WriteLine($"X {x}");
+        }
+
+        public void PrintY()
+        {
+            Console.WriteLine($"Y {y}");
+        }
+
+        public void PrintPoint()
+        {
+            PrintX();
+            PrintY();
+        }
+    }
+}
