7728f5a431992ac6a7697498916e552fe71a5a8b
K Vladimir <korotkih.vladimir.isr21.1@gmail.com>
2022-09-11 18:09:34 +0200

    Создание файла

---
+++ ООП l Перегрузка конструкторов класса/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace ООП_l_Перегрузка_конструкторов_класса
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
