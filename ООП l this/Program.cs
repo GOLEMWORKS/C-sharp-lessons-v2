3cdcfe1b0ac0e6c7649204f601330128fe238224
K Vladimir <korotkih.vladimir.isr21.1@gmail.com>
2022-09-14 20:51:23 +0200

    this создание файла

---
+++ ООП l this/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace ООП_l_this
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
