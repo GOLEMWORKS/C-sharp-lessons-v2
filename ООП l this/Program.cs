4d89a47ff6b4b90ebd79c2e67265671d56f2afa3
K Vladimir <korotkih.vladimir.isr21.1@gmail.com>
2022-09-14 21:14:05 +0200

    Использование в static методах

--- ООП l this/Program.cs
+++ ООП l this/Program.cs
@@ -2,11 +2,17 @@
 {
     class Student
     {
-        public Student(string lastName, DateTime birthday)
+        public Student(string lastName, DateTime birthday):this(lastName) //Использоваие this в конструкторах
         {
             //this - обращается к полям объекта класса внутри него, а не снаружи
-            this.lastName = lastName;
             this.birthday = birthday;
+
+            //Обработка параметра lastName делегируется другому конструктору
+        }
+
+        public Student(string lastName)
+        {
+            this.lastName = lastName;
         }
 
         private string lastName;
@@ -22,6 +28,8 @@
     {
         static void Main(string[] args)
         {
+            //this - нельзя использовать в методах с модификатором static. Static методы не требуют создания объекта (экземпляра) класса (Пример: Console.Writeline)
+
             var studentik = new Student("Иванов", new DateTime(2000, 12, 30));
             studentik.Print();
 
