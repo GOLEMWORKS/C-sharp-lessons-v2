ad033a9a8fd83351a1f6d0cf7fa4fdcb9feb55c9
K Vladimir <korotkih.vladimir.isr21.1@gmail.com>
2022-09-14 21:02:33 +0200

    Коомменты и основное использование this

--- ООП l this/Program.cs
+++ ООП l this/Program.cs
@@ -1,10 +1,32 @@
 ﻿namespace ООП_l_this
 {
+    class Student
+    {
+        public Student(string lastName, DateTime birthday)
+        {
+            //this - обращается к полям объекта класса внутри него, а не снаружи
+            this.lastName = lastName;
+            this.birthday = birthday;
+        }
+
+        string lastName;
+        DateTime birthday;
+
+        public void Print()
+        {
+            Console.WriteLine($"Фамилия {lastName}, Дата рождения {birthday}");
+        }
+    }
+
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var studentik = new Student("Иванов", new DateTime(2000, 12, 30));
+            studentik.Print();
+
+            var studento4ka = new Student("Петрова", new DateTime(2001, 2, 20));
+            studento4ka.Print();
         }
     }
 }
