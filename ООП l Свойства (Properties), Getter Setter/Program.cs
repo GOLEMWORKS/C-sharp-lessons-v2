34d50fc0141b4df441dffc1938bbb02af06f0fde
K Vladimir <korotkih.vladimir.isr21.1@gmail.com>
2022-09-15 20:43:32 +0200

    Базис

---
+++ ООП l Свойства (Properties), Getter Setter/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace ООП_l_Свойства__Properties___Getter_Setter
+{
+    class Point
+    {
+        private int x;
+
+        public void SetX(int x)
+        {
+            this.x = x;
+        }
+
+        public int GetX()
+        {
+            return x;
+        }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
