b9751a2d37e496cd96c181d1127184d01ed56f8d
K Vladimir <korotkih.vladimir.isr21.1@gmail.com>
2022-09-15 21:01:10 +0200

    GET и SET - аксессоры

--- ООП l Свойства (Properties), Getter Setter/Program.cs
+++ ООП l Свойства (Properties), Getter Setter/Program.cs
@@ -2,14 +2,34 @@
 {
     class Point
     {
-        private int x;
+        private int x;  
 
-        public void SetX(int x)
+        private int y; //Сниппет propfull
+
+        public int Y //Properties (Свойства)
         {
+            get { return y; }
+            set { y = value; }
+        }
+
+
+        public void SetX(int x) //Сеттер (Помещает)
+        {
+            if (x < 1)
+            {
+                this.x = 1;
+                return;
+            }
+            if(x > 5)
+            {
+                this.x = 5;
+                return;
+            }
+            
             this.x = x;
         }
 
-        public int GetX()
+        public int GetX()//Геттер (Передаёт)
         {
             return x;
         }
@@ -19,7 +39,12 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var newPoint1 = new Point();
+            newPoint1.SetX(4);
+            newPoint1.Y = 12;
+
+            Console.WriteLine($"X: {newPoint1.GetX()}");
+            Console.WriteLine($"Y: {newPoint1.Y}");
         }
     }
 }
