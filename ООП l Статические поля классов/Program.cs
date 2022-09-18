b7e981cc6b4033de054a8722189c9666e3011945
K Vladimir <korotkih.vladimir.isr21.1@gmail.com>
2022-09-18 16:31:54 +0200

    Работа со swtatic полями

--- ООП l Статические поля классов/Program.cs
+++ ООП l Статические поля классов/Program.cs
@@ -1,10 +1,40 @@
 ﻿namespace ООП_l_Статические_поля_классов
 {
+    class MyClass
+    {
+        public int a;
+
+        public static int b;
+
+        private static int c;
+
+        public void PrintC()
+        {
+            Console.WriteLine(c);
+        }
+
+        public void SetC(int c)
+        {
+            MyClass.c = c; //С this Не работает т.к. нету экземпляра класса
+        }
+    }
+    //Статические переменные не требуют экземпляра класса, а нестатические наоборот! Им нужен экземпляр класса для функционирования!
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            //Поля у объектов независимы друг от друга в оперативной памяти
+            var myClass1 = new MyClass();
+            myClass1.a = 12;
+
+            var myClass2 = new MyClass();
+            myClass2.a = 34;
+
+            //У статических память даётся одна на всех. Каждый объект будет работать с одной
+            //и той же областью в памяти. Нам не требуется создавать экземпляр класса!
+
+            //myClass1.b - не сущаствует, обращаться нужно на уровне самого класса, а не его экземпляра
+            MyClass.b = 32;
         }
     }
 }
