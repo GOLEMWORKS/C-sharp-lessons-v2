b4e429a40fb253cfeb02f7c3a8cf020de120b459
K Vladimir <korotkih.vladimir.isr21.1@gmail.com>
2022-09-18 16:17:20 +0200

    Статические поля классов проект

---
+++ ООП l Статические поля классов/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace ООП_l_Статические_поля_классов
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
