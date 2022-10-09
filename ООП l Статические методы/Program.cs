41e91707e2176e985d65ef0d52a65a0518b19ac7
K Vladimir <korotkih.vladimir.isr21.1@gmail.com>
2022-10-09 14:29:11 +0200

    Статические методы

---
+++ ООП l Статические методы/Program.cs
@@ -0,0 +1,40 @@
+﻿namespace ООП_l_Статические_методы
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Влияние static на свойства и методы класса
+
+            var classExOne = new MyClass(); 
+
+            //classExOne.FooPSV(); - не вызовется, т.к. статические методы не вызываются на уровне экземпляра класса!!!
+            MyClass.FooPSV(); //Вызывается на уровне класса, а не его экземпляра!
+        }
+    }
+
+    class MyClass
+    {
+        private static int firstInt; //ОБЩЕЕ ДЛЯ ВСЕХ ЭКЗЕМПЛЯРОВ КЛАССА
+
+        private int secondInt;
+
+        public static void FooPSV() //Можно использовать метод без создания экземпляра класса. Аналогично общий метод для всех экземпляров класса
+        {
+            Console.WriteLine("FooPSV");
+
+            //В методе можно использовать тоько статические члены класса
+            firstInt = 1;
+            Console.WriteLine(firstInt);
+
+            //Обычно статические методы используются для создания инструметария, на пример: Сортировка, вывод на консоль и т.д.
+            //Теряется возможность использования полиморфизма!!!
+        }
+
+        public void BarPV()
+        {
+            Console.WriteLine("BarPV");
+            Console.WriteLine(firstInt);
+        }
+    }
+}
