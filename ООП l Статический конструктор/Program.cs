1839885f71d08bc37e983f9e40d8eed372c6c2b4
K Vladimir <korotkih.vladimir.isr21.1@gmail.com>
2022-10-20 21:06:40 +0200

    файл создание

---
+++ ООП l Статический конструктор/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace ООП_l_Статический_конструктор
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
