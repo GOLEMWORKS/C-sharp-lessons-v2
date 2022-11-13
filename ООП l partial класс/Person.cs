3fd279bac03f9b7d95010e11d5044e38025a8edd
K Vladimir <korotkih.vladimir.isr21.1@gmail.com>
2022-11-13 14:42:17 +0200

    partial

---
+++ ООП l partial класс/Person.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ООП_l_partial_класс
+{
+    partial class Person //Название класса не зависит от названия файла
+    {
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public Person(string firstName, string lastName) 
+        {
+            FirstName = firstName;
+            LastName = lastName;
+        }
+    }
+}
---
+++ ООП l partial класс/PersonMetods.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ООП_l_partial_класс
+{
+    partial class Person //Название аналогично первому классу, одно пространство имён, ключевое слово partial => это часть класса Person, в которую вынесены методы
+    {
+        public string GetFullName()
+        {
+            return FirstName + " " + LastName;
+        }
+
+        public void PrintFullName()
+        {
+            Console.WriteLine(GetFullName());
+        }
+    }
+}
